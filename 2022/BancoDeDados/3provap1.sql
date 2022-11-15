7335f431e11acbefa2c214a5ddf71e1a8d8385e9
fennec001 <guilhermemollsilva@gmail.com>
2022-11-15 17:15:34 -0300

    Corrections to certain exercises

--- 2022/BancoDeDados/3provap1.sql
+++ 2022/BancoDeDados/3provap1.sql
@@ -11,7 +11,8 @@ CREATE TABLE editora( cd_editora int primary key, nm_editora varchar(100) );
 CREATE TABLE tipo_obra( tp_obra int primary key, ds_tipo varchar(30) );
 
 CREATE TABLE obra_autor( obra_cd_obr int, autor_cd_au int, primary key (obra_cd_obr, autor_cd_au) ); alter TABLE obra add constraint primary key (cd_obra); ALTER TABLE obra_autor ADD CONSTRAINT foreign key (obra_cd_obr) references obra(cd_obra); alter TABLE obra_autor add constraint foreign key (autor_cd_au) references autor(cd_autor); alter TABLE obra add constraint foreign key (cd_editora) references editora(cd_editora), add constraint foreign key(tp_obra)references tipo_obra(tp_obra);
---b) ALTER TABLE autor add constraint unique (nm_autor);
+--b) 
+ALTER TABLE autor add constraint unique (nm_autor);
 --c)
 -- 10 autores, 3 tipo obras, 5 editoras, 10 obras
 INSERT INTO editora(cd_editora, nm_editora) VALUES(1, 'penguin');
@@ -37,7 +38,7 @@ INSERT INTO OBRA(cd_obra, nm_titulo, ds_edicao, dt_publicacao, ds_sinopse, tp_ob
 INSERT INTO OBRA(cd_obra, nm_titulo, ds_edicao, dt_publicacao, ds_sinopse, tp_obra, cd_editora) VALUES (3, 'Stone Ocean', '2019', '2012-03-03', 'Cali prison', 3, 3);
 INSERT INTO OBRA(cd_obra, nm_titulo, ds_edicao, dt_publicacao, ds_sinopse, tp_obra, cd_editora) VALUES (4, 'Naruto', '2000', '2001-01-09', 'Ninjas', 2, 4);
 INSERT INTO OBRA(cd_obra, nm_titulo, ds_edicao, dt_publicacao, ds_sinopse, tp_obra, cd_editora) VALUES (5, 'My Hero Academia', '2018', '2015-09-02', 'Children with power', 1, 5);
-INSERT INTO OBRA(cd_obra, nm_titulo, ds_edicao, dt_publicacao, ds_sinopse, tp_obra, cd_editora) VALUES (6, 'Baki', '2003', '1992-02-02', 'Extreme violence', 2, 3;)
+INSERT INTO OBRA(cd_obra, nm_titulo, ds_edicao, dt_publicacao, ds_sinopse, tp_obra, cd_editora) VALUES (6, 'Baki', '2003', '1992-02-02', 'Extreme violence', 2, 3);
 INSERT INTO OBRA(cd_obra, nm_titulo, ds_edicao, dt_publicacao, ds_sinopse, tp_obra, cd_editora) VALUES (7, 'Kimetsu no Yaiba', '2018', '2015-01-01', 'Demons and Katanas', 3, 1);
 INSERT INTO OBRA(cd_obra, nm_titulo, ds_edicao, dt_publicacao, ds_sinopse, tp_obra, cd_editora) VALUES (8, 'Death Note', '2017', '2008-02-02', 'Death in a notebook', 3, 4);
 INSERT INTO OBRA(cd_obra, nm_titulo, ds_edicao, dt_publicacao, ds_sinopse, tp_obra, cd_editora) VALUES (9, 'Akame Ga Kill', '2016', '2003-02-02', 'so many deaths', 2, 5);
@@ -57,7 +58,7 @@ INSERT INTO obra_autor(obra_cd_obr, autor_cd_au) VALUES (10, 7);
 INSERT INTO obra_autor(obra_cd_obr, autor_cd_au) VALUES (11, 8);
 INSERT INTO obra_autor(obra_cd_obr, autor_cd_au) VALUES (3, 9);
 INSERT INTO obra_autor(obra_cd_obr, autor_cd_au) VALUES (4, 10);
-INSERT INTO obra_autor(obra_cd_obr, autor_cd_au) VALUES (5, 6);
+INSERT INTO obra_autor(obra_cd_obr, autor_cd_au) VALUES (5, 6);       
 --d) Listar os dados das obras (título, data de publicação, nome da editora e descrição do tipo da obra);
 
 SELECT  nm_titulo     AS titulo
@@ -69,7 +70,7 @@ JOIN editora e
 ON (obra.cd_editora = e.cd_editora)
 JOIN tipo_obra tob
 ON (obra.tp_obra = tob.tp_obra);
---e) Listar o título da(s) obra(s) do tipo "livro" e que foram publicados no ano de 2022;
+--e) Listar o título da(s) obra(s) do tipo "romance" e que foram publicados no ano de 2022;
 
 SELECT  ds_tipo       AS tipo
        ,nm_titulo     AS titulo
